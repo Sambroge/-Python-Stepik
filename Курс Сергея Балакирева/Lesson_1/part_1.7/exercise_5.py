4e45a6cb6b1becf9faafbff7d6ca86d3567ec459
Sambroge <92974992+Sambroge@users.noreply.github.com>
2022-07-19 08:30:20 +0300

    Create

---
+++ Курс Сергея Балакирева/Lesson_1/part_1.7/exercise_5.py
@@ -0,0 +1,21 @@
+class Application:
+    def __init__(self, name, blocked=False):
+        self.name = name
+        self.blocked = blocked
+
+
+class AppStore:
+    def __init__(self):
+        self.applications = []
+        
+    def add_application(self, app):
+        self.applications.append(app)
+        
+    def remove_application(self, app):
+        self.applications.remove(app)
+        
+    def block_application(self, app):
+        app.blocked = True
+        
+    def total_apps(self):
+        return(len(self.applications))
