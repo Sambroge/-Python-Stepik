67ca48a18a0808350ee0558ec9f0cf8e08d03185
Sambroge <92974992+Sambroge@users.noreply.github.com>
2022-08-01 14:46:58 +0300

    Create excersie_5.py

---
+++ Курс Сергея Балакирева/Lesson_1/part_1.4/excersie_5.py
@@ -0,0 +1,21 @@
+class Translator:
+    d = {}
+    def add(self, eng, rus):
+        self.d.setdefault(eng, []).append(rus)
+    def remove(self, eng):
+        self.d.pop(eng)
+    def translate(self, eng):
+        return self.d[eng]
+
+tr = Translator()
+tr.add('tree', 'дерево')
+tr.add('car', 'машина')
+tr.add('car', 'автомобиль')
+tr.add('leaf', 'лист')
+tr.add('river', 'река')
+tr.add('go', 'идти')
+tr.add('go', 'ехать')
+tr.add('go', 'ходить')
+tr.add('milk', 'молоко')
+tr.remove('car')
+print(*tr.translate('go'))
